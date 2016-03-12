4bf9d7a5ea10f6a618b4d434cec4232a40ef8d24
k <k@xxx.local>
2016-03-13 01:36:50 +0500

    Add models Question and Answer

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,22 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField()
+#    rating = models.IntgerField()
+    rating = models.IntegerField()
+    author = models.ForeignKey(User, related_name='question_author',  null=True, 
+                    on_delete=models.SET_NULL)
+    like = models.ManyToManyField(User, related_name='question_like')
+    
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    question = models.ForeignKey(Question, 
+                    on_delete=models.CASCADE)
+    author = models.ForeignKey(User, null=True, 
+                    on_delete=models.SET_NULL)
+    
+    
--- file.bash
+++ file.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
+./mange.py syncdb
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/test
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/ask
