87c8b37a1cfce5de1c6b34faf96ed8f29f225513
k <k@xxx.local>
2016-03-13 07:15:21 +0500

    bugfix in initial script

--- file.bash
+++ file.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
-mysql -uroot -p -e "create database box character set utf8"
-mysql -uroot -p -e "create user 'box@localhost' identified by 'boxboxbox'"
-mysql -uroot -p -e "grant all on box.* to 'box'"
-./mange.py syncdb
+mysql -uroot -e "create database box character set utf8"
+mysql -uroot -e "create user 'box@localhost' IDENTIFIED BY 'boxboxbox'"
+mysql -uroot -e "grant all privileges on box.* to 'box'"
+./manage.py syncdb
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/test
-sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/ask
+sudo ln -sf /home/box/web/etc/ask.conf /etc/gunicorn.d/ask
 sudo service nginx restart
 sudo service gunicorn restart
