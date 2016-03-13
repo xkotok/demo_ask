bf76957c89d54829e5cee03a5fcbf0a280718c70
k <k@xxx.local>
2016-03-13 06:51:55 +0500

    Using MySQL database

--- file.bash
+++ file.bash
@@ -1,4 +1,7 @@
 #!/bin/bash
+mysql -uroot -p -e "create database box character set utf8"
+mysql -uroot -p -e "create user 'box@localhost' identified by 'boxboxbox'"
+mysql -uroot -p -e "grant all on box.* to 'box'"
 ./mange.py syncdb
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/test
