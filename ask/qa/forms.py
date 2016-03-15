418b0ef6e150b24fc42a3795dcb7eec417551a7a
k <k@xxx.local>
2016-03-15 19:38:24 +0500

    f__ git )

---
+++ ask/qa/forms.py
@@ -0,0 +1,23 @@
+# -- coding: utf-8 --
+from django import forms
+from models import Question, Answer
+
+class AskForm(forms.Form):
+    title = forms.CharField(max_length=255, widget=forms.Textarea)
+    text = forms.CharField(widget=forms.Textarea)
+    
+    def __init__(self, *args, **kwargs):
+#        self._user = user
+        super(AskForm, self).__init__(*args, **kwargs)
+    def save(self):
+#        self.data['author'] = self._user
+        question = Question(**self.cleaned_data)
+        question.save()
+        return question
+class AnswerForm(forms.Form):
+    text = forms.CharField(widget=forms.Textarea)
+    question = forms.IntegerField(widget=forms.HiddenInput)
+    def save(self):
+        answer = Answer(**self.cleaned_data)
+        answer.save()
+        return answer #why?
